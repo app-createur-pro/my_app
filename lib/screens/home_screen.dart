bf8698d35bf1bbcae277cb62f7cdefe820aa7c8c
Captain Web <contact@captain-web.fr>
2021-04-11 18:55:45 +0200

    consumer

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -19,7 +19,8 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
 
-    final _authenticationProvider = Provider.of<AuthenticationProvider>(context);
+    final _authenticationProvider = Provider.of<AuthenticationProvider>(context, listen: false);
+    print("rebuild home");
 
     return Scaffold(
       appBar: AppBar(
@@ -29,31 +30,54 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(_authenticationProvider.isConnected ? "Vous êtes connecté" : "Vous n'êtes pas connecté"),
-              SizedBox(height: 15,),
-              ElevatedButton(
-                  onPressed: () {
-
-                    if(_authenticationProvider.isConnected) {
-                      _authenticationProvider.disconnect();
-                    } else {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => SignInScreen()
-                          )
-                      );
-                    }
-
-                  },
-                  style: ElevatedButton.styleFrom(
-                    primary: _authenticationProvider.isConnected ? Colors.red : Colors.blue
-                  ),
-                  child: Text(_authenticationProvider.isConnected ? "Se deconnecter" : "Se connecter")
+              Image.asset("assets/images/banana.jpg"),
+              Consumer<AuthenticationProvider>(
+                  builder: (context, value, child) => NewWidget(authenticationProvider: _authenticationProvider
+                )
               ),
             ],
           )
       ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class NewWidget extends StatelessWidget {
+  const NewWidget({
+    Key key,
+    @required AuthenticationProvider authenticationProvider,
+  }) : _authenticationProvider = authenticationProvider, super(key: key);
+
+  final AuthenticationProvider _authenticationProvider;
+
+  @override
+  Widget build(BuildContext context) {
+    print("rebuild new widget");
+    return Column(
+      children: [
+        Text(_authenticationProvider.isConnected ? "Vous êtes connecté" : "Vous n'êtes pas connecté"),
+        SizedBox(height: 15,),
+        ElevatedButton(
+            onPressed: () {
+
+              if(_authenticationProvider.isConnected) {
+                _authenticationProvider.disconnect();
+              } else {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => SignInScreen()
+                    )
+                );
+              }
+
+            },
+            style: ElevatedButton.styleFrom(
+              primary: _authenticationProvider.isConnected ? Colors.red : Colors.blue
+            ),
+            child: Text(_authenticationProvider.isConnected ? "Se deconnecter" : "Se connecter")
+        ),
+      ],
     );
   }
 }
